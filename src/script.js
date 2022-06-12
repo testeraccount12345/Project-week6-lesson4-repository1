1560242b577251bd34576c63daff60160d15553e
DJ <macbp2019@hotmail.com>
2022-06-12 13:38:43 -0700

    HTML & JS removed C & F converter, JS added forecast description, CSS added design elements

--- src/script.js
+++ src/script.js
@@ -42,42 +42,12 @@ function handleSubmit(event) {
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
-// Celsius to Fahrenheit coverter
-function convertToFahrenheit(event) {
-  event.preventDefault();
-
-  let temperatureElement = document.querySelector("#temperature");
-  // add the active class to celsius link
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", convertToFahrenheit);
-
-// Fahrenheit to Celsius converter
-function convertToCelsius(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-
-let celsiusTemperature = null;
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", convertToCelsius);
-
 // function that receives the forecast coordinates
 function getForecast(coordinates) {
   let apiKey = "7be1b7858b81bbf663eb3e6f25fa5f88";
   let units = "metric";
   let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${units}`;
 
-  console.log(apiURL);
   axios.get(apiURL).then(displayForecast);
 }
 
@@ -117,7 +87,6 @@ function formatDateForecast(timestamp) {
 // looping weatherforecast function
 function displayForecast(response) {
   let forecast = response.data.daily;
-  console.log(forecast);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -131,14 +100,19 @@ function displayForecast(response) {
   <span id="forecast-days">
     ${formatDateForecast(forecastDay.dt)}</span></div>
     <div>
+    
     <img src="http://openweathermap.org/img/wn/${
       forecastDay.weather[0].icon
     }@2x.png" id="forecast-icon" />
+      <span id="forecast-description">${forecastDay.weather[0].main}</span> 
+
+    
     </div>
     <div id="forecast-highlow">
     <span id="forecast-high">${Math.round(forecastDay.temp.max)}°C</span>
     <span id="forecast-low">${Math.round(forecastDay.temp.min)}°C</span> 
-     </div>
+ 
+    </div>
   </div>
 `;
     }
